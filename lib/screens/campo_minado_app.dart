9e961cd6d374f6c60104f851c6330e381ec326b8
Arthur Camurça <arthur.04.pb@gmail.com>
2020-04-07 11:32:26 -0300

    campo pra stateful  mét reiniciar, abrir, alternar

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,44 +1,94 @@
 import 'package:campo_minado/components/resultado_widget.dart';
-import 'package:campo_minado/components/campo_widget.dart';
+import 'package:campo_minado/components/tabuleiro_widget.dart';
+import 'package:campo_minado/models/tabuleiro.dart';
 import '../models/campo.dart';
 import 'package:flutter/material.dart';
 import '../models/explosao_exception.dart';
 
-class CampoMinadoApp extends StatelessWidget {
+class CampoMinadoApp extends StatefulWidget {
+  @override
+  _CampoMinadoAppState createState() => _CampoMinadoAppState();
+}
 
-  final Campo campo = Campo(linha: 0, coluna: 0);
+class _CampoMinadoAppState extends State<CampoMinadoApp> {
+  bool _venceu;
+  Tabuleiro _tab;
 
-  void reiniciar(){
-    print('reiniciar...');
+  void reiniciar() {
+    setState(() {
+      _venceu = null;
+      _tab.reiniciar();
+    });
   }
 
-  void _abrir(Campo campo){
-    print('abrir...');
+  void _abrir(Campo campo) {
+    if (_venceu != null) {
+      return;
+    }
+    setState(() {
+      try {
+        campo.abrir();
+        if (_tab.resolvido) {
+          _venceu = true;
+        }
+      } on ExplosaoException {
+        _venceu = false;
+        _tab.revelarBombas();
+      }
+    });
   }
 
-  void _alternarMarcacao(Campo campo){
-    print('alternarMarcacao...');
+  void _alternarMarcacao(Campo campo) {
+    if (_venceu != null) {
+      return;
+    }
+    setState(() {
+      campo.alternarMarcacao();
+      if (_tab.resolvido) {
+        _venceu = true;
+      }
+    });
+  }
+
+  Tabuleiro _getTab(double largura, double altura) {
+    if (_tab == null) {
+      int qtdColunas = 15;
+      double tamanhoCampo = largura / qtdColunas;
+      int qtdLinhas = (altura / tamanhoCampo).floor();
+
+      _tab = Tabuleiro(
+        linhas: qtdLinhas,
+        colunas: qtdColunas,
+        qtdeBombas: 20,
+      );
+    }
+    return _tab;
   }
 
   @override
   Widget build(BuildContext context) {
-    try {
-    campo.minar();
-    campo.abrir();
-  } on ExplosaoException{}
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: ResultadoWidget(
-          venceu: true,
+          venceu: _venceu,
           onReiniciar: reiniciar,
         ),
-        body: Container(child: CampoWidget(
-          campo: campo,
-          onAbrir: _abrir,
-          onAlternarMarcacao: _alternarMarcacao,
-        )),
+        body: Container(
+          color: Colors.grey,
+          child: LayoutBuilder(
+            builder: (ctx, constraints) {
+              return TabuleiroWidget(
+                tabuleiro: _getTab(
+                  constraints.maxWidth,
+                  constraints.maxHeight,
+                ),
+                onAbrir: _abrir,
+                onAlternarMarcacao: _alternarMarcacao,
+              );
+            },
+          ),
+        ),
       ),
     );
   }
